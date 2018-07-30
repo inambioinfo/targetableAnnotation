68cd3da7d53ec19da623b178a9b5e5fe0b60de34
nickchim <nichchim@haverford.org>
2018-07-30 12:27:27 -0400

    Editing

--- targetApply.R
+++ targetApply.R
@@ -1,24 +1,20 @@
 Target_genes <<- read.csv("Target_List.tsv",sep="\t",stringsAsFactors = FALSE)
 
 #Function
-targetApply <- function(in) {
-  in$Targetable <- FALSE
-  in$Drug_name <- NA
-  in$Drug_chembl_id <- NA
+targetApply <- function(input) {
+  input$Targetable <- FALSE
+  input$Drug_name <- NA
+  input$Drug_chembl_id <- NA
 
-  return(as.data.frame(t(apply(in1,FUN=function(x) {
+  return(as.data.frame(t(apply(input,FUN=function(x) {
     if (x["H_Gene"] %in% Target_genes$Trgt_Genes) {
       x["Targetable"] <- TRUE
-
       index <- which(Target_genes$Trgt_Genes == x["H_Gene"])
-
-
       x["Drug_name"] <- Target_genes$Drug_name[index]
-
       x["Drug_chembl_id"] <- Target_genes$Drug_chembl_id[index]
 
     }
     return(x)
-  },MARGIN = 1)))
+  },MARGIN = 1))))
  
 }
